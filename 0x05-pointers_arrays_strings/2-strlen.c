5cff381e1c89aa3179c36f5c4e9283c0e3f70f9c
Liul Abera <liulabera43@gmail.com>
2023-02-27 13:10:39 -0800

    success

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,18 +6,15 @@
  * Return: length
  */
 
-
- int _strlen(char *s)
+int _strlen(char *s)
 {
 	int longi = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
 	return (longi);
-}
-
-	
+}	
 
